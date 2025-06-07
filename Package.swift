0edea67959ed914d515624d754710b2336a2703a
Hyomin Koo <me@eatsteak.dev>
2025-06-07 22:14:40 +0900

    chore: add `swift-docc-plugin` for generating docs

--- Package.swift
+++ Package.swift
@@ -14,6 +14,9 @@ let package = Package(
             name: "Rusaint",
             targets: ["Rusaint", "RusaintFFI"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.4")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
